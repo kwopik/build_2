7f20400f32da4a5f6ddab6a54b562c6e3f7c32f6
kwopik <51060618+kwopik@users.noreply.github.com>
2023-11-15 21:40:56 +0300

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 func main() {
     r := gin.Default()
     r.GET("/", func(c *gin.Context) {
-        c.String(200, "Hello!!!!!test2!!!!!!!!!!!1, Go with Docker8086 test branch!")
+        c.String(200, "Hello!!!!!test2!!!!!!!!!!!1, Go with Docker8086 test branchtest!")
     })
     r.Run(":8080")
 }
